c289306ef3ad9c6e677438fe1863d06b35e11f99
cadecameron <cade.cameron@gmail.com>
2020-01-18 16:18:42 -0800

    UPDATED RepoList.jsx to output repo info into a table

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const RepoList = (props) => (
+const RepoList = ({ repos }) => (
   <div>
     <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    There are {repos.length} repos.
+    <table>
+      <tbody>
+        {repos.map((repo) => <tr>{repo.owner.login}</tr>)}
+      </tbody>
+    </table>
   </div>
 )
 
